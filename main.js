462b025b7436136f8eb6d116d9ef47e87e0b4590
Clarence Saxby <csaxby318@gmail.com>
2021-03-09 10:37:59 -0500

    created function to redirect customer to airline site base on carrier name

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const returnDateTextBox = document.getElementById("returnDateTextBox")
 const submitBtn = document.getElementById("submitBtn")
 
 const displayFlight = document.getElementById("displayFlight")
+const flightLink = document.getElementById("flightLink")
 
 function fetchFlight(from, to, leaveDate, returnDate) {
 
@@ -21,6 +22,7 @@ function fetchFlight(from, to, leaveDate, returnDate) {
 }).then((result) => {
     console.log(result);
     flightDisplay(result)
+    carrierRedirectLink(result.Carriers[0].Name)
 }).catch(err => {
 	console.error(err);
 });
@@ -30,7 +32,7 @@ function flightDisplay(result) {
 
     let date = result.Quotes[0].OutboundLeg.DepartureDate
     let formattedDate = date.slice(6, 10) + "-" + date.slice(0, 4)
-
+    
     displayFlight.innerHTML = `
                             <div class="flightInfo">
                                 <h2>${result.Carriers[0].Name}</h2>
@@ -38,6 +40,7 @@ function flightDisplay(result) {
                                 <p>From: ${result.Places[0].Name} (${result.Places[0].IataCode})</p>
                                 <p>To: ${result.Places[1].Name} (${result.Places[1].IataCode})</p>
                                 <p>$${result.Quotes[0].MinPrice}</p>
+                                
                             </div>
                          `
 }
@@ -55,3 +58,58 @@ submitBtn.addEventListener('click', function() {
     leaveDateTextBox.value = ''
     returnDateTextBox.value = ''
 })
+
+function carrierRedirectLink(carrier) {
+
+    if(carrier == "Spirit Airlines") {
+        flightLink.innerHTML = `<a href="https://www.spirit.com/">Buy Ticket</a>`
+    }else if(carrier == "Frontier Airlines"){
+        flightLink.innerHTML = `<a href="https://www.flyfrontier.com/">Buy Ticket</a>`
+    }else if(carrier == "American Airlines"){
+        flightLink.innerHTML = `<a href="https://www.aa.com/homePage.do">Buy Ticket</a>`
+    }else if(carrier == "Delta"){
+        flightLink.innerHTML = `<a href="https://www.delta.com/">Buy Ticket</a>`
+    }else if(carrier == "United"){
+        flightLink.innerHTML = `<a href="https://www.united.com/">Buy Ticket</a>`
+    }else if(carrier == "jetBlue"){
+        flightLink.innerHTML = `<a href="https://www.jetblue.com/">Buy Ticket</a>`
+    }else if(carrier == "Southwest Airlines"){
+        flightLink.innerHTML = `<a href="https://www.southwest.com/">Buy Ticket</a>`
+    }else if(carrier == "Alaska Airlines"){
+        flightLink.innerHTML = `<a href="https://www.alaskaair.com/">Buy Ticket</a>`
+    }else if(carrier == "Hawaiian Airlines"){
+        flightLink.innerHTML = `<a href="https://www.hawaiianairlines.com/">Buy Ticket</a>`
+    }else if(carrier == "Virgin Atlantic"){
+        flightLink.innerHTML = `<a href="https://www.virginatlantic.com/">Buy Ticket</a>`
+    }else if(carrier == "Air Canada"){
+        flightLink.innerHTML = `<a href="https://www.aircanada.com/us/en/aco/home.html">Buy Ticket</a>`
+    }else if(carrier == "Virgin America"){
+        flightLink.innerHTML = `<a href="https://www.virginatlantic.com/">Buy Ticket</a>`
+    }else if(carrier == "Air France"){
+        flightLink.innerHTML = `<a href="https://www.airfrance.com/">Buy Ticket</a>`
+    }else if(carrier == "Qatar Airways"){
+        flightLink.innerHTML = `<a href="https://www.qatarairways.com/en-us/homepage.html">Buy Ticket</a>`
+    }else if(carrier == "KLM Royal Dutch Airlines"){
+        flightLink.innerHTML = `<a href="https://www.klm.com/">Buy Ticket</a>`
+    }else if(carrier == "Emirates"){
+        flightLink.innerHTML = `<a href="https://www.emirates.com/us/english/">Buy Ticket</a>`
+    }else if(carrier == "Allegiant Air"){
+        flightLink.innerHTML = `<a href="https://www.allegiantair.com/">Buy Ticket</a>`
+    }else if(carrier == "Japan Airlines"){
+        flightLink.innerHTML = `<a href="https://www.jal.co.jp/jp/en/">Buy Ticket</a>`
+    }else if(carrier == "Air New Zealand"){
+        flightLink.innerHTML = `<a href="https://www.airnewzealand.com/">Buy Ticket</a>`
+    }else if(carrier == "Fly Dubai"){
+        flightLink.innerHTML = `<a href="https://www.flydubai.com/">Buy Ticket</a>`
+    }else if(carrier == "Egyptair"){
+        flightLink.innerHTML = `<a href="https://www.egyptair.com/">Buy Ticket</a>`
+    }else if(carrier == "Malaysia Airlines"){
+        flightLink.innerHTML = `<a href="https://www.malaysiaairlines.com/">Buy Ticket</a>`
+    }else if(carrier == "Turkish Airlines"){
+        flightLink.innerHTML = `<a href="https://www.turkishairlines.com/">Buy Ticket</a>`
+    }else if(carrier == "AirAsia"){
+        flightLink.innerHTML = `<a href="https://www.airasia.com/en/gb">Buy Ticket</a>`
+    }else {
+        flightLink.innerHTML = `<a href="https://www.skyscanner.com/">Buy Ticket</a>`
+    }
+}
